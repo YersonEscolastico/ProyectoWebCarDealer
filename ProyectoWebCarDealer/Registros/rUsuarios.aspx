e453247d6e079702377b05fa1672ae07087da6e1
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-08-27 00:25:54 +0800

    Agregando Registros de Usuarios

---
+++ ProyectoWebCarDealer/Registros/rUsuarios.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="rUsuarios.aspx.cs" Inherits="ProyectoWebCarDealer.Registros.rUsuarios" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+ <%--  --%>
+    <div class="content">
+    <div id ="Datos">
+
+        <p style="color:#ffd800;text-align:center; font-size:larger">REGISTRO DE USUARIOS</p>
+
+ <asp:Label ID="Label2" runat="server" Text="ID"></asp:Label>
+                <asp:TextBox class="form-control" ID="IdNumericUpDown" runat="server" type="number" min="0" onkeypress="return isNumberKey(event)"></asp:TextBox>
+
+<asp:Button ID="buscarButton" runat="server" Text="BUSCAR" class="btn btn-dark btn-lg text-center my-3"  style="text-align: right;width:345px" OnClick="buscarButton_Click1"/>
+
+<asp:Label ID="Label3" runat="server" Text="Nombres"></asp:Label>
+                <asp:TextBox class="form-control" ID="NombresTextBox" runat="server" onkeypress="return isLetterKey(event)" ></asp:TextBox>
+
+<asp:Label ID="Label4" runat="server" Text="Email"></asp:Label>
+                <asp:TextBox class="form-control" ID="EmailTextBox"  pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{1,5})$" runat="server"> </asp:TextBox>
+
+<asp:Label ID="Label5" runat="server" Text="Usuario"></asp:Label>
+                <asp:TextBox class="form-control" ID="usuario" runat="server" onkeypress="if (event.keyCode == 32) event.returnValue = false"> </asp:TextBox>
+
+<asp:Label ID="Label6" runat="server" Text="Clave"></asp:Label>
+                <asp:TextBox class="form-control" ID="ClaveTextBox" runat="server" type="password"> </asp:TextBox>
+
+<asp:Label ID="Label7" runat="server" Text="Confirmar Clave"></asp:Label>
+                <asp:TextBox class="form-control" ID="ConfirmarClaveTextBox" runat="server"  type="password"> </asp:TextBox>
+
+ <asp:Label ID="Label8" runat="server" Text="Fecha Registro"></asp:Label>
+                <asp:TextBox class="form-control" ID="FechaRegistros" type="date" runat="server" ></asp:TextBox>
+
+                          <div style="text-align: right;width:340px"> 
+<asp:Button ID="nuevoButton" runat="server" Text="NUEVO" class="btn btn-primary btn-lg text-center my-3" OnClick="nuevoButton_Click"/>         
+ <asp:Button ID="GuardarButton" runat="server" Text="Guardar" class="btn btn-success btn-lg text-center my-3" OnClick="GuardarButton_Click1"/>
+ <asp:Button ID="Eliminar" runat="server" Text="Eliminar" class="btn btn-danger btn-lg text-center my-3" OnClick="Eliminar_Click"/>
+
+</div>
+</div>
+</div>
+</asp:Content>
---
+++ ProyectoWebCarDealer/Registros/rUsuarios.aspx.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using Entidades;
+using DAL;
+using BLL;
+
+namespace ProyectoWebCarDealer.Registros
+{
+    public partial class rUsuarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            FechaRegistros.Text = DateTime.Today.ToString("yyyy-MM-dd");
+        }
+        private void Limpiar()
+        {
+            IdNumericUpDown.Text = "0";
+            NombresTextBox.Text = string.Empty;
+            EmailTextBox.Text = string.Empty;
+            usuario.Text = string.Empty;
+            ConfirmarClaveTextBox.Text = string.Empty;
+        }
+
+        protected void nuevoButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private Usuarios LlenarClase()
+        {
+            Usuarios usuarios = new Usuarios();
+
+            int.TryParse(IdNumericUpDown.Text, out int idx);
+            usuarios.Nombre = NombresTextBox.Text;
+            usuarios.Email = EmailTextBox.Text;
+            usuarios.Usuario = usuario.Text;
+            usuarios.Clave = ClaveTextBox.Text;
+            return usuarios;
+        }
+
+        private void LlenarCampos(Usuarios usuarios)
+        {
+            IdNumericUpDown.Text = Convert.ToString(usuarios.UsuarioId);
+            NombresTextBox.Text = usuarios.Nombre;
+            EmailTextBox.Text = usuarios.Email;
+            usuario.Text = usuarios.Usuario;
+            usuario.Text = usuarios.Clave;
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+
+            if (string.IsNullOrWhiteSpace(IdNumericUpDown.Text) || string.IsNullOrWhiteSpace(NombresTextBox.Text) || string.IsNullOrWhiteSpace(usuario.Text) || string.IsNullOrWhiteSpace(ClaveTextBox.Text) || string.IsNullOrWhiteSpace(EmailTextBox.Text) || ConfirmarClaveTextBox.Text != ClaveTextBox.Text)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "Pop", "Validacion()", true);
+                paso = false;
+            }
+            return paso;
+        }
+
+        public static bool RepetirUser(string descripcion)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.Usuarios.Any(p => p.Usuario.Equals(descripcion)))
+                {
+                    paso = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        public static bool RepetirEmail(string descripcion)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.Usuarios.Any(p => p.Email.Equals(descripcion)))
+                {
+                    paso = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        private bool ValidarRepetir()
+        {
+            bool paso = true;
+
+            if (RepetirUser(usuario.Text))
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "Pop", "Repeticion()", true);
+                paso = false;
+            }
+            if (RepetirEmail(EmailTextBox.Text))
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "Pop", "Repeticion()", true);
+                paso = false;
+            }
+            return paso;
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            RepositorioBase<Usuarios> Repositorio = new RepositorioBase<Usuarios>();
+            Usuarios usuarios = Repositorio.Buscar(Convert.ToInt32(IdNumericUpDown.Text));
+            return (usuarios != null);
+        }
+
+        protected void GuardarButton_Click1(object sender, EventArgs e)
+        {
+            RepositorioBase<Usuarios> db = new RepositorioBase<Usuarios>();
+            Usuarios usuarios;
+
+            bool paso = false;
+            if (!Validar())
+                return;
+
+            usuarios = LlenarClase();
+
+            int.TryParse(IdNumericUpDown.Text, out int idx);
+            if (idx == 0)
+            {
+                if (!ValidarRepetir())
+                    return;
+
+                paso = db.Guardar(usuarios);
+                Limpiar();
+            }
+            else
+            {
+                if (!ExisteEnLaBaseDeDatos())
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "Pop", "Validacion()", true);
+                    return;
+                }
+                paso = db.Modificar(usuarios);
+                Limpiar();
+            }
+
+            if (paso)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "Pop", "Exito()", true);
+                return;
+            }
+            else
+                ClientScript.RegisterStartupScript(this.GetType(), "Pop", "SinExito()", true);
+        }
+
+        protected void buscarButton_Click1(object sender, EventArgs e)
+        {
+            RepositorioBase<Usuarios> db = new RepositorioBase<Usuarios>();
+            int id;
+            Usuarios usuarios = new Usuarios();
+            int.TryParse(IdNumericUpDown.Text, out int idx);
+
+            usuarios = db.Buscar(idx);
+            if (usuarios != null)
+                LlenarCampos(usuarios);
+            else
+                ClientScript.RegisterStartupScript(this.GetType(), "Pop", "SinExito()", true);
+        }
+
+
+        protected void Eliminar_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Usuarios> Repositorio = new RepositorioBase<Usuarios>();
+            int idx;
+            int.TryParse(IdNumericUpDown.Text, out idx);
+
+            var usuario = Repositorio.Buscar(idx);
+            if (usuario != null)
+            {
+                if (Repositorio.Eliminar(idx))
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "Pop", "Exito()", true);
+                    Limpiar();
+                }
+                else
+                    ClientScript.RegisterStartupScript(this.GetType(), "Pop", "Validacion()", true);
+            }
+            else
+                ClientScript.RegisterStartupScript(this.GetType(), "Pop", "SinExito()", true);
+        }
+    }
+}
