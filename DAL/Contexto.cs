5c4fd14d0a68dc1d1d4fef327984464ecc5af51c
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-08-27 00:10:56 +0800

    Agregando Contexto de Usuarios

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
