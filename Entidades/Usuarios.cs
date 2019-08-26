513ae0ddfdd29257d4e4805bf81436bcb7824e28
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-08-26 23:56:58 +0800

    Agregando Entidad Usuarios

---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuarios
+    {
+
+        [Key]
+
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Nombre = string.Empty;
+            Email = string.Empty;
+            Usuario = string.Empty;
+            Clave = string.Empty;
+            Fecha = DateTime.Now;
+        }
+    }
+}
